671054dd17a57dace9d935c5a51767eb02216514
AlexChehov <0650343005id@gmail.com>
2022-11-05 16:17:48 +0200

    test

--- js/script.js
+++ js/script.js
@@ -9,4 +9,29 @@ $('.about-section_coffe_line').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3
-  });
+    // responsive: [
+    //     {
+    //         breakpoint: 1440,
+    //         settings: {
+    //             slidesToShow: 3,
+    //             slidesToScroll: 3,
+    //             infinite: true,
+    //             dots: true
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 746,
+    //         settings: {
+    //             slidesToShow: 2,
+    //             slidesToScroll: 2
+    //         }
+    //     },
+    //     {
+    //         breakpoint: 640,
+    //         settings: {
+    //             slidesToShow: 1,
+    //             slidesToScroll: 1
+    //         }
+    //     }
+    // ]
+});
