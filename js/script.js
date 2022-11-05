dd9d96b7d150784ff6e236a3052158f7063d4417
AlexChehov <0650343005id@gmail.com>
2022-11-05 15:54:05 +0200

    test

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ burger.addEventListener('click', (e) => {
     burger.classList.toggle('active-burger')
     menuList.classList.toggle('list-active')
 })
+
+$('.about-section_coffe_line').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
